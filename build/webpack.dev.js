a46420329cb0368cdadaf5a6e5380691e4651c2b
caokejian_i <caokejian_i@didiglobal.com>
2024-04-09 14:45:41 +0800

    update: package

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -2,9 +2,10 @@ const { merge } = require('webpack-merge')
 const base = require('./webpack.base.js')
 
 module.exports = merge(base, {
-  mode: 'development', // 开发模式
+  stats: 'errors-warnings',
+  mode: 'development',
   devServer: {
-	  open: true, // 编译完自动打开浏览器
+	  open: true,
     port: 8080,
   },
   module: {
