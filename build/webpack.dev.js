72ad1f95adc28fd7048e2802ae92b9f3b8c4f9b0
caokejian <1849201815@qq.com>
2024-05-21 01:44:15 +0800

    log: log提示

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -2,6 +2,7 @@
 const path = require('path')
 const { merge } = require('webpack-merge')
 const base = require('./webpack.base.js')
+const { webpackEntry } = require('./compile.js')
 
 module.exports = merge(base, {
   stats: 'errors-warnings',
@@ -10,9 +11,39 @@ module.exports = merge(base, {
     static: {
       directory: path.join(__dirname, '../dist'),
     },
-    open: true,
+    // open: true,
     port: 8080,
     historyApiFallback: true,
+    setupMiddlewares: (middlewares, devServer) => {
+      devServer.compiler.hooks.done.tap('done', (stats) => {
+        console.log('\x1b[34m','  /\\_/\\  /\\_/\\  /\\_/\\  /\\_/\\  /\\_/\\  /\\_/\\  /\\_/\\  /\\_/\\  /\\_/\\  /\\_/\\', '\x1b[0m')
+        console.log('\x1b[34m',' ( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )', '\x1b[0m')
+        console.log('\x1b[34m','  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <', '\x1b[0m')
+        console.log('\x1b[34m','  /\\_/\\                                                          /\\_/\\', '\x1b[0m')
+        console.log('\x1b[34m',' ( o.o )   ', '\x1b[33m', '________  ________  ___       ___  ________      ','\x1b[34m','( o.o )', '\x1b[0m')
+        console.log('\x1b[34m','  > ^ <   ', '\x1b[33m', '|\\   ____\\|\\   __  \\|\\  \\     |\\  \\|\\   ___  \\     ','\x1b[34m','> ^ <', '\x1b[0m')
+        console.log('\x1b[34m','  /\\_/\\   ', '\x1b[33m', '\\ \\  \\___|\\ \\  \\|\\  \\ \\  \\    \\ \\  \\ \\  \\\\ \\  \\    ','\x1b[34m','/\\_/\\', '\x1b[0m')
+        console.log('\x1b[34m',' ( o.o )   ', '\x1b[33m', '\\ \\  \\    \\ \\  \\\\\\  \\ \\  \\    \\ \\  \\ \\  \\\\ \\  \\  ','\x1b[34m','( o.o )', '\x1b[0m')
+        console.log('\x1b[34m','  > ^ <     ', '\x1b[33m', '\\ \\  \\____\\ \\  \\\\\\  \\ \\  \\____\\ \\  \\ \\  \\\\ \\  \\  ','\x1b[34m','> ^ <', '\x1b[0m')
+        console.log('\x1b[34m','  /\\_/\\      ', '\x1b[33m', '\\ \\_______\\ \\_______\\ \\_______\\ \\__\\ \\__\\\\ \\__\\ ','\x1b[34m','/\\_/\\', '\x1b[0m')
+        console.log('\x1b[34m',' ( o.o )      ', '\x1b[33m', '\\|_______|\\|_______|\\|_______|\\|__|\\|__| \\|__|','\x1b[34m','( o.o )', '\x1b[0m')
+        console.log('\x1b[34m','  > ^ <                                                          > ^ <', '\x1b[0m')
+        console.log('\x1b[34m','  /\\_/\\  /\\_/\\  /\\_/\\  /\\_/\\  /\\_/\\  /\\_/\\  /\\_/\\  /\\_/\\  /\\_/\\  /\\_/\\', '\x1b[0m')
+        console.log('\x1b[34m',' ( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )', '\x1b[0m')
+        console.log('\x1b[34m','  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <', '\x1b[0m')
+        
+        console.log('🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉');
+        console.log(`🎉🎉🎉  共成功打包${Object.entries(webpackEntry).length}个组件，地址如下：`)
+        console.log('🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉');
+        let count = 1
+        for (const [name] of Object.entries(webpackEntry)) {
+          console.log('\x1b[33m', `${count}. ${name}:`, '\x1b[33m', `http://localhost:8080/${name}`, '\x1b[0m');
+          count++
+        }
+      })
+
+      return middlewares
+    }
   },
   module: {
     rules: [
