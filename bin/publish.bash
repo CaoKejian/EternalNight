e8c51c458154589be257a30c0966dc2209fdbc15
caokejian <1849201815@qq.com>
2024-06-16 20:38:40 +0800

    feat: 添加发布脚本

--- bin/publish.bash
+++ bin/publish.bash
@@ -6,6 +6,9 @@ function title {
   echo 
 }
 
+current_version=$(npm show @caokejian/nuwa-components version)
+echo "当前 @caokejian/nuwa-components 版本: $current_version"
+
 title "更新 @caokejian/nuwa-components 版本"
 read -p "选择大、中、小版本的迭代 (b/m/s): " choice
 # 判断用户的选择
@@ -22,3 +25,6 @@ else
   echo "无效选择, 默认更新小版本。"
   npm version patch
 fi
+
+updated_version=$(npm show @caokejian/nuwa-components version)
+echo "更新完成：@caokejian/nuwa-components 当前版本为: $updated_version"
